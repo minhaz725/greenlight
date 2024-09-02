b39867ad01c0a5e052540d7b309c140e5addc8d9
minhaz <minhaz725@gmail.com>
2024-09-03 02:15:48 +0600

    page 29

---
+++ cmd/api/movies.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, _ := app.readIDParam(r)
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
